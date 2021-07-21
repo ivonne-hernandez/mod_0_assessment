f8fbb28a781f423e21f6b734ae793f90ef952ba4
Ivonne Hernandez <ivonne702@gmail.com>
2021-07-21 15:38:28 -0600

    Add model for McCarran Airport

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var mccarranAirport = {
+  fullName: "McCarran International Airport",
+  abbreviation: "LAS",
+  numberOfVisitors: 1232324,
+  yearBuilt: 1942,
+  isOpen: true,
+  hasAirplanes: true,
+  terminals: ["Terminal 1", "Terminal 2", "Terminal 3"],
+  airlines: ["Southwest", "Spirit", "Frontier"]
+};
