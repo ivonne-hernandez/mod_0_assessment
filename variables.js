8259373883fb620c3fdd10d211758cb668fc2de4
Ivonne Hernandez <ivonne702@gmail.com>
2021-07-21 15:31:04 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var famousPerson = "Jared Leto";
+var isActive = true;
+
+famousPerson = "Henry Cavill";
+
+var num3 = 30;
+var num9 = 34;
+var sum = num3 + num9;
+
+var animals = ["cheetah", "lion", "elephant"];
+animals.shift();
+console.log(famousPerson.length);
