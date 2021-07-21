782ed0c1eeb24b0c0348911962eb2e506ad25a13
Ivonne Hernandez <ivonne702@gmail.com>
2021-07-21 15:44:53 -0600

    Add model for my car

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var myCorolla = {
+  color: "red",
+  make: "Toyota",
+  year: 2016,
+  mpg: 21,
+  isRegistered: true,
+  isPaidOff: true,
+  citiesTraveled: ["Denver", "Las Vegas", "Breckenridge"],
+  features: ["air conditioning", "bluetooth", "tinted windows"]
+};
